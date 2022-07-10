c0c5c2fab82105a8f640a37267fd9812a7356d80
Jerome Luce <jerome.luce@protonmail.com>
2022-07-10 08:17:29 +0200

    Move creation of restaurant from google data to the restaurant model to keep the code DRY

--- app/jobs/build_group_restaurants_list_job.rb
+++ app/jobs/build_group_restaurants_list_job.rb
@@ -13,30 +13,12 @@ class BuildGroupRestaurantsListJob < ApplicationJob
             end
             restaurants["results"].each do |restaurant|
                 unless all_group_restaurants.find_by(google_place_id: restaurant["place_id"])&.update_attribute(:currently_in_radius, true)
-                    Restaurant.create(
-                        name: restaurant["name"],
-                        address: restaurant["vicinity"],
-                        lat: restaurant["geometry"]["location"]["lat"],
-                        lng: restaurant["geometry"]["location"]["lng"],
-                        distance_in_km: Geocoder::Calculations.distance_between([grouplat, grouplng], [restaurant["geometry"]["location"]["lat"].to_f, restaurant["geometry"]["location"]["lng"].to_f], { units: :km })&.round(3),
-                        google_rating: restaurant["rating"],
-                        google_place_id: restaurant["place_id"],
-                        group_id: group.id
-                    )
+                    Restaurant.create_from_google_data(restaurant, group.id, grouplat, grouplng)
                 end
             end
         else
             restaurants["results"].each do |restaurant|
-                Restaurant.create(
-                    name: restaurant["name"],
-                    address: restaurant["vicinity"],
-                    lat: restaurant["geometry"]["location"]["lat"],
-                    lng: restaurant["geometry"]["location"]["lng"],
-                    distance_in_km: Geocoder::Calculations.distance_between([grouplat, grouplng], [restaurant["geometry"]["location"]["lat"].to_f, restaurant["geometry"]["location"]["lng"].to_f], { units: :km })&.round(3),
-                    google_rating: restaurant["rating"],
-                    google_place_id: restaurant["place_id"],
-                    group_id: group.id
-                )
+                Restaurant.create_from_google_data(restaurant, group.id, grouplat, grouplng)
                 
             end
         end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,17 @@ class Restaurant < ApplicationRecord
   acts_as_votable
   belongs_to :group
   validates :name, :address, :lat, :lng, :google_rating, :google_place_id, presence: true
+
+  def self.create_from_google_data(restaurant, group_id, grouplat, grouplng)
+    Restaurant.create(
+      name: restaurant["name"],
+      address: restaurant["vicinity"],
+      lat: restaurant["geometry"]["location"]["lat"],
+      lng: restaurant["geometry"]["location"]["lng"],
+      distance_in_km: Geocoder::Calculations.distance_between([grouplat, grouplng], [restaurant["geometry"]["location"]["lat"].to_f, restaurant["geometry"]["location"]["lng"].to_f], { units: :km })&.round(3),
+      google_rating: restaurant["rating"],
+      google_place_id: restaurant["place_id"],
+      group_id: group_id
+    )
+  end
 end
